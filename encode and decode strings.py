5a3716c89ea0fe56270a7f31ad1b90a72f6153d8
Eric Gong <11supereric@gmail.com>
2023-06-28 19:14:10 -0400

    Create encode and decode strings.py

---
+++ encode and decode strings.py
@@ -0,0 +1,33 @@
+class Solution:
+    """
+    @param: strs: a list of strings
+    @return: encodes a list of strings to a single string.
+    """
+    def encode(self, strs):
+        # write your code here
+        bigString = ""
+        for i in strs:
+            lenght = str(len(i))
+            bigString = bigString + lenght + "@" + i
+        return bigString
+    """
+    @param: str: A string
+    @return: decodes a single string to a list of strings
+    """
+    def decode(self, str):
+        # write your code here
+            listOfStrings = []
+        numLetters = ""
+        index = 0
+        while index < len(str):
+            if str[index] == "@":
+                length = int(numLetters)
+                word = str[index+1 : index+1+length]
+                listOfStrings.append(word)
+                numLetters = ""
+                index = index+1+length
+            else:
+                numLetters = numLetters + str[index]
+                index += 1
+
+        return listOfStrings
