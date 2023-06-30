cbadd4328e3270844676d578101dcf428c4c1903
Eric Gong <11supereric@gmail.com>
2023-06-29 21:11:17 -0400

    Create min stack.py

---
+++ min stack.py
@@ -0,0 +1,36 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        self.stack.append(val)
+        if self.minStack and self.minStack[-1] < val:
+            self.minStack.append(self.minStack[-1])
+        else:
+            self.minStack.append(val)
+  
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
