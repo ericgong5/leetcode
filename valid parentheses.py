75072a33e1509c6d75ac3a563b114494681cdcfc
Eric Gong <11supereric@gmail.com>
2024-10-11 01:10:09 -0400

    Update valid parentheses.py

--- valid parentheses.py
+++ valid parentheses.py
@@ -1,3 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {')' : '(', '}' : '{', ']' : '['}
+        stack = []
+        
+        for char in s:
+            if char in mapping:
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(char)
+
+        return True if not stack else False
+
+
+
+
+use mapping instead
+
+
+
+
+
+
+
+###
 class Solution(object):
     def isValid(self, s):
         """
@@ -23,3 +51,4 @@ class Solution(object):
         if stack[-1] == 'a':
             return True
         return False
+###
